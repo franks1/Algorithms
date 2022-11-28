143ab5fa8669f933944ac03102ec6fa6088bbed7
Francis Banini <fkbani@gmail.com>
2022-11-28 22:34:31 +0000

    Added Roman numerals

--- AlgoList/Algorithms.cs
+++ AlgoList/Algorithms.cs
@@ -1,14 +1,16 @@
 ﻿namespace AlgoList;
 
-public static class PascalTriangle
+public static class Algorithms
 {
-    private static int[,] pascal = new int[7, 7];
-    public static void PascalTriangleBuild(int series=7)
+    //pascal
+
+    public static void PascalTriangle(int series)
     {
+        int[,] pascal = new int[series, series];
 
         for (int i = 0; i < series; i++)
         {
-            for (int k = series; k > i; k+=-1)
+            for (int k = series; k > i; k += -1)
             {
                 Console.Write(" ");
             }
@@ -25,8 +27,93 @@ public static class PascalTriangle
                 }
                 Console.Write(pascal[i, j]+" ");
             }
-
-            Console.ReadLine();
+            Console.WriteLine();
         }
     }
+
+    public static void FibonucciSeries(int series)
+    {
+        List<int> _list = new();
+
+        if (series <= 1)
+        {
+            _list.Add(0);
+            return;
+        }
+        
+        _list.Add(0);
+        _list.Add(1);
+        int a = 0, b = 1, c = 0;
+        for (int i = 2; i < series; i++)
+        {
+            c = a + b;
+            _list.Add(c);
+            a = b;
+            b = c;
+        }
+
+        foreach(var item in _list)
+        {
+            Console.Write(item+",");
+        }
+    }
+
+    public static string RomanNumerals(int numerals)
+    {
+        string output = string.Empty;
+        Dictionary < string, int > romanNumbersDictionary = new() {
+            {
+                "I",
+                1
+            }, {
+                "IV",
+                4
+            }, {
+                "V",
+                5
+            }, {
+                "IX",
+                9
+            }, {
+                "X",
+                10
+            }, {
+                "XL",
+                40
+            }, {
+                "L",
+                50
+            }, {
+                "XC",
+                90
+            }, {
+                "C",
+                100
+            }, {
+                "CD",
+                400
+            }, {
+                "D",
+                500
+            }, {
+                "CM",
+                900
+            }, {
+                "M",
+                1000
+            }
+        };
+
+        foreach (var item in romanNumbersDictionary.Reverse())
+        {
+            if (numerals <= 0) break;
+            while (numerals >= item.Value)
+            {
+                output += item.Key;
+                numerals -= item.Value;
+            }
+        }
+        return output;
+    }
+    
 }
--- AlgoList/Program.cs
+++ AlgoList/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using AlgoList;
+
 Console.WriteLine("Hello, World!");
+
+//Algorithms.PascalTriangle(7);
+var numerals=Algorithms.RomanNumerals(9);
+Console.Write(numerals);
+// var result=Algorithms.RomanNumerals(100);
+// Console.Write(result);
